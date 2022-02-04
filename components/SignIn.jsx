f1478ed683d940e89f93614f385cb2efdf5a64b6
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-02-04 19:49:31 +0100

    prima dei cambiamenti

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
+
+import { Formik } from "formik";
+
+import SignInForm from "./SignInForm";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const onSubmit = ({ values }) => {
+  console.log("bruuuuh", values);
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
---
+++ components/SignInForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import FormikTextInput from "./FormikTextInput";
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" />
+      <Pressable onPress={onSubmit}>
+        <Text>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default SignInForm;
