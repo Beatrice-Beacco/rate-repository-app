69dac1dc5051781cf3b7c87226f0bcc9af9d93d2
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-02-13 15:36:38 +0100

    manca l'alert

--- src/hooks/useDeleteReview.js
+++ src/hooks/useDeleteReview.js
@@ -1,8 +1,18 @@
 import { useMutation } from "@apollo/client";
 import { DELETE_REVIEW } from "../graphql/mutations";
+import { LOGGED_USER } from "../graphql/queries";
 
 const useDeleteReview = () => {
-  const [mutate, result] = useMutation(DELETE_REVIEW);
+  const [mutate, result] = useMutation(DELETE_REVIEW, {
+    refetchQueries: [
+      {
+        query: LOGGED_USER,
+        variables: {
+          includeReviews: true,
+        },
+      },
+    ],
+  });
 
   const deleteReview = async (deleteReviewId) => {
     await mutate({
